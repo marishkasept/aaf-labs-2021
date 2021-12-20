c736d9948e076d97ced6a706148ff0c73c037b59
Мария <martynovalala@gmail.com>
2021-12-20 17:32:34 +0200

    funcs to check the commands are created + gitignore file

--- mmartynova/Parser.cs
+++ mmartynova/Parser.cs
@@ -12,6 +12,86 @@ namespace ua.lab.oaa.Components{
             var words = clearCommand.Split(' ', '\t', '\r', '\n');
             bool success = false;
             string userChoice = words[0].ToUpper();
+
+            foreach (Commands command in (Commands[]) Enum.GetValues(typeof(Commands)))
+            {
+                if (command.ToString() == userChoice )
+                {
+                    success = true;
+                    choice = command;
+                }
+
+                if (!success)
+                {
+                    Console.WriteLine("Error! There is no command like this. Try again");
+                    return 0;
+                }
+
+                switch (choice)
+                {
+                    case Commands.EXIT:
+					return -1;
+                    break;
+
+                    case Commands.CREATE:
+                        if (!IsCorrectWordsCount(words, 2)){
+                            return 0;
+                        }
+
+                        trie = new Trie(null, words[1]);
+					    triesInCache.Add(trie);
+                        ErrorNoTrie();
+                        break;
+
+                    case Commands.INSERT:
+                        if (!IsCorrectWordsCount(words, 3)){
+                            return 0;
+                        }
+
+                        if(triesInCache.Exists(x => x.name == words[1])){
+                            triesInCache.Find(x => x.name == words[1]).AddWord(words[2].Trim('"'));
+                        }else{
+                            ErrorNoTrie();
+                        }
+                        break;
+
+                    case Commands.PRINT_TREE:
+                        if (!IsCorrectWordsCount(words, 2)){
+                            return 0;
+                        }
+
+                        if(triesInCache.Exists(x => x.name == words[1])){
+                            triesInCache.Find(x => x.name == words[1]).PrintTrie();
+                        }else{
+                            ErrorNoTrie();
+                            return 0;
+                        }
+                        Console.WriteLine("Success!");
+                        break;
+
+                    case Commands.CONTAINS:
+					    if (!IsCorrectWordsCount(words, 3)){
+                            return 0;
+                        }
+
+                        if(triesInCache.Exists(x => x.name == words[1])){
+                    	    trie = triesInCache.Find(x => x.name == words[1]);
+                    	    var result = trie.ContainsWord(words[2].Trim('"'));
+						    Console.WriteLine($"The result is {result}");
+					    }else{
+						    ErrorNoTrie();
+					    }
+                        break;
+
+                    case Commands.SEARCH:
+                        break;
+
+                    default:
+                        Console.WriteLine("Unhandled Error! Try again");
+                        break;
+
+                }
+            }
         }
     }
 }
