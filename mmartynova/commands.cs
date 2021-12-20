f8d87fceee8dcd749c6124ae015eb4b5966928a8
Мария <martynovalala@gmail.com>
2021-12-20 18:15:45 +0200

    commands constants initialised

---
+++ mmartynova/commands.cs
@@ -0,0 +1,13 @@
+namespace ua.lab.oaa.Constants
+{
+    public enum Commands
+    {
+	DEFAULT = -1,
+    EXIT = 0,
+    CREATE = 1,
+    INSERT = 2,
+    PRINT_TREE = 3,
+    CONTAINS = 4,
+    SEARCH = 5,
+    }
+}
